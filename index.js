a9ccb9ced9e7f76e62519c26257de058b04f1979
Mark Shkreli <m.shkreli96@icloud.com>
2022-10-10 21:17:19 -0400

    commit tree e6f4bec97ace47f2f5fda477420d605f4af1a56d

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 12
+let num4 = 3
+
+const multiply = num1 * num2;
+
+let random;
+
+random = Math.floor(Math.random() * 10);
+
+Number.isInteger(random);
+
+const mod = num3 / num4;
+
+let max;
+
+max = Math.max(5,10,15,20);
